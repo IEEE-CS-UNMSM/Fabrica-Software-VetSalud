ee24a34645bb4a5c0e22ec15250238b731f24431
Cristhian Paolo <cristh715@gmail.com>
2023-10-27 15:42:20 -0500

    Añadiendo la clase usuario

---
+++ Desarrollo/Backend/VetSaludFS/src/main/java/com/mycompany/vetsaludfs/model/Usuario.java
@@ -0,0 +1,90 @@
+package com.mycompany.vetsaludfs.model;
+
+public class Usuario {
+    protected int id;
+    protected String dni;
+    protected String nombres;
+    protected String apellidos;
+    protected String direccion;
+    protected int celular;
+    protected String email;
+    protected String pass;
+    protected String rol;
+    
+    public Usuario (){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    
+}
