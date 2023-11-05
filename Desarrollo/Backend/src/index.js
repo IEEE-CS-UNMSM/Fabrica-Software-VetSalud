35e9de1830ca22bcef0edba5b288485ac676a1b2
Cristhian Paolo <cristh715@gmail.com>
2023-11-04 22:26:59 -0500

    Realizando cambio a node.js

---
+++ Desarrollo/Backend/src/index.js
@@ -0,0 +1,35 @@
+const express = require ('express')
+const morgan = require ('morgan')
+
+const app = express();
+
+app.set('port', process.env.PORT || 4000);
+
+app.use(morgan('dev'));
+
+app.listen(app.get('port'), () =>{
+    console.log('Servidor iniciado en el puerto', app.get('port') )
+})
+
+
+
+/*
+var mysql = require ('mysql2')
+
+var conexion = mysql.createConnection({
+    host: 'localhost',
+    database: 'bd_veterinaria',
+    user: 'root',
+    password: ''
+});
+
+conexion.connect(function (error){
+    if (error){
+        throw error;
+    }else{
+        console.log('Conexion exitosa');
+    }
+});
+
+conexion.end();
+*/
