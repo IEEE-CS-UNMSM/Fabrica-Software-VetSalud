315737bd6bf62f9d9abde01fb2aff20492d201cb
Cristhian Paolo <cristh715@gmail.com>
2023-11-11 14:11:11 -0500

    Corrigiendo errores

--- Desarrollo/src/view/clientlist.js
+++ Desarrollo/src/view/clientlist.js
@@ -36,3 +36,16 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+fetch('/obtener-usuarios')
+  .then(response => response.json())
+  .then(data => {
+    const tablaUsuarios = document.getElementById('tablaUsuarios');
+    data.forEach(usuario => {
+      const fila = `<tr class="tabla-row"><td class="tabla-cell">${usuario.NOMBRES_USUARIO}` +
+      `</td><td class="tabla-cell">${usuario.DNI_USUARIO}</td>` +
+      `</td><td class="tabla-cell">${usuario.CELULAR_USUARIO}</td></tr>`;
+      tablaUsuarios.innerHTML += fila;
+    });
+  })
+  .catch(error => console.error('Error al obtener usuarios:', error));
