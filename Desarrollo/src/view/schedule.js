8ecbe2d22af4b0f8b6ba3674cb4aa69b18413ad5
Cristhian Paolo <cristh715@gmail.com>
2023-11-12 11:12:55 -0500

    Corrigiendo formato de fecha de citas

--- Desarrollo/src/view/schedule.js
+++ Desarrollo/src/view/schedule.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
-        fetch('/obtener-citas')
-        .then(response => response.json())
-        .then(data => {
-            // Formatear los datos obtenidos y agregarlos a calendarEvents
-            const eventos = data.map(cita => {
-                return {
-                    id: cita.ID_CITA,
-                    name: "Nombre",
-                    date: cita.FECHA_HORA_CITA,
-                    description: cita.MOTIVO_CITA,
-                    type: "schedule appointment"
-                };
-            });
+    fetch('/obtener-citas')
+    .then(response => response.json())
+    .then(data => {
+        // Formatear los datos obtenidos y agregarlos a calendarEvents
+        const eventos = data.map(cita => {
+            return {
+                id: cita.ID_CITA,
+                name: "Nombre",
+                date: cita.FECHA_HORA_CITA,
+                description: cita.MOTIVO_CITA,
+                type: "schedule appointment"
+            };
+        });
 
-            $('#calendar').evoCalendar({
-                language: "es",
-                theme: "Midnight Blue",
-                format: 'dd/mm/yyyy',
-                eventHeaderFormat: "dd MM yyyy",
-                calendarEvents: eventos
-            });
-        })
-        .catch(error => console.error('Error al obtener eventos:', error));
+        $('#calendar').evoCalendar({
+            language: "es",
+            theme: "Midnight Blue",
+            format: 'mm/dd/yyyy',
+           eventHeaderFormat: "dd MM yyyy",
+            calendarEvents: eventos
+        });
+    })
+    .catch(error => console.error('Error al obtener eventos:', error));
 })
