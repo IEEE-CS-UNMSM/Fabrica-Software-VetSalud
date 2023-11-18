a8132d549a1f1bd17c92dc770d99f1d438984a91
Cristhian Paolo <cristh715@gmail.com>
2023-11-18 12:06:47 -0500

    Organizanco carpetas

--- Desarrollo/src/routes/routes.js
+++ Desarrollo/src/routes/routes.js
@@ -8,6 +8,6 @@ router.get('/', (req, res) => {
     res.sendFile('./src/view/index.html');
   });
 
-  router.get('/obtener-usuarios', ClienteController.obtenerUsuarios);
+router.get('/obtener-usuarios', ClienteController.obtenerUsuarios);
 router.get('/obtener-citas', CitasController.obtenerCitas);
 module.exports = router;
