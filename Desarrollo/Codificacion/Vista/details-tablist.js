862b2eedfb2256ef8c445be518409567d0f93ffa
ImanolY <148709430+ImanolY@users.noreply.github.com>
2023-12-13 15:25:31 -0500

    Detalles Historial

---
+++ Desarrollo/Codificacion/Vista/details-tablist.js
@@ -0,0 +1,14 @@
+
+const aceptar = document.querySelector("#aceptar");
+const mensaje = document.querySelector("#mensaje");
+const detallesArray = document.querySelectorAll(".detalles-abrir");
+
+detallesArray.forEach((detalles) => {
+    detalles.addEventListener("click", () => {
+        mensaje.showModal();
+    });
+});
+
+aceptar.addEventListener("click", () => {
+    mensaje.close();
+});
